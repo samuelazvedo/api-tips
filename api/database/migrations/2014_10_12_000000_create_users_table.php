af523fa687cd70264e1bacf33f279641af3c5435
samuelazvedo <samuelsousa71@gmail.com>
2024-01-04 22:04:02 -0300

    Update 2014_10_12_000000_create_users_table.php

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+<<<<<<< Updated upstream:api/database/migrations/2014_10_12_000000_create_users_table.php
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+=======
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'ongoing', 'completed'])->default('pending');
+>>>>>>> Stashed changes:api/database/migrations/2024_01_04_222027_create_tasks_table.php
             $table->timestamps();
         });
     }
